88b0bd27f5853974e862f6df437e5593d17c03cd
cpulover <justacpulover@gmail.com>
2020-08-04 09:03:22 +0200

    Format

--- php/introduction.php
+++ php/introduction.php
@@ -1,22 +1,22 @@
 <!-- INTRODUCTION -->
 <div class="jumbotron-fluid" id="home">
-    <div class="container" id="introduction">
-      <br><br><br><br>
-      <div class="display-1">Hello world!</div>
-      <h3>I am <span class="text-danger">Hieu Ngo</span>, just a noob developer :P</h3>
-      <br>
-      <p>
-        <a href="https://www.linkedin.com/in/cpulover/" target="_blank">
-          <i class="fa fa-linkedin-square"></i>
-        </a>
-        <a href="https://github.com/cpulover" target="_blank">
-          <i class="fa fa-github-square"></i>
-        </a>
-        <a href="https://www.instagram.com/justacpulover/?hl=en" target="_blank">
-          <i class="fa fa-instagram"></i>
-        </a>
-      </p>
+  <div class="container" id="introduction">
+    <br><br><br><br>
+    <div class="display-1">Hello world!</div>
+    <h3>I am <span class="text-danger">Hieu Ngo</span>, just a noob developer :P</h3>
+    <br>
+    <p>
+      <a href="https://www.linkedin.com/in/cpulover/" target="_blank">
+        <i class="fa fa-linkedin-square"></i>
+      </a>
+      <a href="https://github.com/cpulover" target="_blank">
+        <i class="fa fa-github-square"></i>
+      </a>
+      <a href="https://www.instagram.com/justacpulover/?hl=en" target="_blank">
+        <i class="fa fa-instagram"></i>
+      </a>
+    </p>
 
-    </div>
   </div>
-  <!-- END OF INTRODUCTION -->
+</div>
+<!-- END OF INTRODUCTION -->
