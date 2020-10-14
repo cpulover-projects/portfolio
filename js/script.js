fd3d308f1b32d09f56b34574b4a5d00d3c2849e7
Hieu Ngo <justacpulover@gmail.com>
2020-10-14 22:52:58 +0200

    Add "Go to top" button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 $(document).ready(function () {
-    "use strict";
+    // "use strict";
 
     $('span.link-text').hover(function () {
         $(this).toggleClass('text-danger');
-    })
+    });
 
     $('body').scrollspy({
         target: '#navbar',
         offset: 20
-    })
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > window.innerHeight/8) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
 
     $("form").submit(function (e) {
         //stop default submission for authentication
