bd7d2482c38f988b62f66a15d6eec7e28b0bb1cc
Hieu Ngo <justacpulover@gmail.com>
2020-10-30 15:31:59 +0100

    Update About content

--- php/sections/about.php
+++ php/sections/about.php
@@ -7,35 +7,49 @@
   <br><br>
   <div class="container">
     <div class="row ">
-      <div class="col-lg">
+      <div class="col-lg-5">
         <div class="row d-flex justify-content-center">
           <img id="avt" src="images/avt.jpeg" class="rounded-circle" alt="">
         </div>
         <br>
         <div class="row d-flex justify-content-center">
-          <h5>"Some kinda inspired quotes here..."</h5>
+          <h5>"A kinda inspired quote here..."</h5>
         </div>
         <br>
       </div>
-      <div class="col-lg">
+      <div class="col-lg-7">
         <div id="introduction-text">
-        My name is Hieu Ngo. I am a software engineering student who passionate about everything tech.
+        <i class="fa fa-user"> General</i>
+        <br>
+        My name is Hieu Ngo.
+        I am a software engineering student who passionate about everything tech.
         <br><br>
-        My specialization is back-end development using Java Spring or LAMP stack. I have experiences working with API, microservices, cloud projects. I am also working on my front-end skills, expanding my horizon to Angular and React.
+        <i class="fa fa-code"> Skills</i>
+        <br>
+        My specialization is back-end development using Java Spring or LAMP stack.
+        I have experiences working with projects involving database, API, microservices, cloud.
+        I am also working on my front-end skills, expanding my horizon to Angular and React.
         <br><br>
-        In my free time, I enjoy creating games, especially VR related (I do not play games though :v); playing with Arduino; playing musical instruments and composing music.
+        <i class="fa fa-heart"> Hobbies</i>
+        <br>
+        In my free time, I enjoy creating games 🎮 (I do not play games though  🤣), messing up with Arduino and AI 🤖, hacking (ethically) 🔓, cooking (crappy) food 🍳, playing musical instruments 🎹 🎸 and composing (crappy) music 🎵.
+        <br><br>
+        <i class="fa fa-bed"> Dream</i>
+        <br>
+        The final goal of my life is simulating entire world in VR where human can customize everything and achieve whatever they want while staying away from COVID. 
+        If you are purchasing the same goal, let's get in touch maybe we can help each other 😉.
         </div>
 
         <!-- <?php
-          createProgressBar("Java", "85");
-          createProgressBar("Java", "15");
-          createProgressBar("Java", "45");
-          createProgressBar("Java", "35");
-          createProgressBar("Java", "65");
-          createProgressBar("Java", "85");
-          createProgressBar("Java", "45");
-          createProgressBar("Java", "15");
-        ?> -->
+createProgressBar("Java", "85");
+createProgressBar("Java", "15");
+createProgressBar("Java", "45");
+createProgressBar("Java", "35");
+createProgressBar("Java", "65");
+createProgressBar("Java", "85");
+createProgressBar("Java", "45");
+createProgressBar("Java", "15");
+?> -->
       </div>
     </div>
   </div>
